8c2725b9e41196fb4cacc0b5f1b7499f9544cd32
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-02-08 01:12:10 +0200

    firstDone

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+const startButton = document.querySelector('button[data-start]')
+
+const stopButton = document.querySelector('[data-stop]')
+const bodyEl = document.querySelector('body')
+let timerId = null
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+stopButton.disabled = true
+
+startButton.addEventListener('click', () => {
+    startButton.disabled = true
+    stopButton.disabled = false
+     timerId = setInterval(() => {
+        const randomColorValue = getRandomHexColor()
+        bodyEl.style.backgroundColor = randomColorValue
+    }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+    startButton.disabled = false
+    stopButton.disabled = true
+    clearInterval(timerId)
+
+})
+
+
