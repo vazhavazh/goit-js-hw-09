8c767f596f76f21172567f34c2e5e7bbd503cbc3
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-02-08 11:03:16 +0200

    08.02update

---
+++ src/js/02-timer.js
@@ -0,0 +1,7 @@
+// Описан в документации
+// import flatpickr from "flatpickr";
+// // Дополнительный импорт стилей
+// import "flatpickr/dist/flatpickr.min.css";
+
+// const el = document.getElementById("datetime-picker")
+
